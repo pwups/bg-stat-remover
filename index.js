8eb0f377e6592c16f5161cfa144fb797b15ee260
pwups <aylapmacc@gmail.com>
2025-02-15 09:46:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["bad thinking diary!"];
-const statusTypes = ['idle'];
+const statusMessages = ["miel's tickets ★"];
+const statusTypes = ['dnd'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
