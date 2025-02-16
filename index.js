06009df97e9480e4682d9c696b220d8de584477b
pwups <aylapmacc@gmail.com>
2025-02-16 18:35:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["over swanlake academy ✧"];
-const statusTypes = ['dnd'];
+const statusMessages = ["yuna's dms"];
+const statusTypes = ['online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
